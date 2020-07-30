0f0d19fbb9f7f1a0e939a0e2f596bdbe0f7a9198
azureovo <56991219+azureovo@users.noreply.github.com>
2020-07-30 23:57:02 +0800

    Create ARMS1.C

---
+++ ARMS1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+void main()
+{
+int number;
+int pos,ten_pow;
+int arm_no;
+int temp,i;
+float expon;
+char line[100];
+
+}
